1107b139db5cbecfcfc2695c12e978e8ba838c7c
bingqichen <bingqichen@live.cn>
2017-06-01 23:57:41 +0800

    171

---
+++ 171.js
@@ -0,0 +1,31 @@
+/**
+171. Excel Sheet Column Number
+
+Related to question Excel Sheet Column Title
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function (s) {
+  return s.split('')
+    .map(item => (item.charCodeAt() - 64))
+    .reduce((acc, val) => (acc * 26 + val), 0);
+};
+
+console.log(titleToNumber('AB'));
