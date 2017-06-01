7ea847b14f4e770db0c56569ea2d26e1839af10f
bingqichen <bingqichen@live.cn>
2017-06-02 00:16:39 +0800

    增加一种方法

--- 171.js
+++ 171.js
@@ -22,10 +22,18 @@ Special thanks to @ts for adding this problem and creating all test cases.
  * @param {string} s
  * @return {number}
  */
-var titleToNumber = function (s) {
+var titleToNumber2 = function (s) {
   return s.split('')
     .map(item => (item.charCodeAt() - 64))
     .reduce((acc, val) => (acc * 26 + val), 0);
+};
+
+var titleToNumber = function (s) {
+  var result = 0;
+  for (var i = s.length - 1; i >= 0; i--) {
+    result = result + (s[i].charCodeAt() - 64) * Math.pow(26, s.length - i - 1);
+  }
+  return result;
 };
 
 console.log(titleToNumber('AB'));
