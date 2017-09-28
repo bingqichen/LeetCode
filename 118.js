1da7e43ab28a0106d85c0ead3cfb2aa4b392ecde
bingqichen <bingqichen@live.cn>
2017-09-28 18:03:59 +0800

    118

--- 118.js
+++ 118.js
@@ -20,5 +20,26 @@ Return
  * @return {number[][]}
  */
 var generate = function(numRows) {
+  if (numRows === 0) {
+    return [];
+  }
 
+  var res = [];
+  for(var i = 0; i < numRows; i++){
+    var line = [];
+    var lineLen = i + 1;
+    for (var j = 0; j < lineLen; j++){
+      if (j == 0 || j == lineLen - 1) {
+        line.push(1);
+      } else {
+        line.push(res[i - 1][j - 1] + res[i - 1][j]);
+      }
+    }
+
+    res.push(line);
+  }
+
+  return res;
 };
+
+console.log(generate(5));
