7d44a9b9eee28e0a15e81ada825552f82db1f3a4
bingqichen <bingqichen@live.cn>
2021-05-28 09:37:13 +0800

    二叉树修复

--- Binary_Search_Tree.js
+++ Binary_Search_Tree.js
@@ -11,148 +11,181 @@
  * 单个节点
  */
 class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = this.right = null;
+  constructor (val) {
+    this.val = val
+    this.left = this.right = null
   }
 }
 
 class Tree {
-  constructor(data = null) {
-    this.root = null;
-    if (data) {
-      this.root = new Node(data);
+  constructor (val = null) {
+    this.root = null
+    if (val) {
+      this.root = new Node(val)
     }
   }
 
-  insert(data) {
-    const node = new Node(data);
-
-    if (this.root === null) {
-      this.root = node;
-    } else {
-      this.insertNode(this.root, node);
-    }
-  }
-  insertNode(node, newNode) {
-    if (node.data > newNode.data) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  remove(data) {
-    this.root = this.removeNode(this.root, data);
-  }
-  removeNode(node, data) {
-    if (node === null) {
-      return null;
-    } else if (node.data === data) {
-      if (node.left === null && node.right === null) {
-        return null;
-      } else if (node.left === null) {
-        return node.right;
-      } else if (node.right === null) {
-        return node.left;
-      }
-    } else if (node.data > data) {
-      node.left = this.removeNode(node.left, data);
-      return node;
-    } else {
-      node.right = this.removeNode(node.right, data);
-      return node;
-    }
-  }
-
-  find(data) {
-    let node = this.root;
-    while(true){
-      if (node === null){
-        return null;
-      } else if (data === node.data) {
-        return node;
-      }
-      node = data < node.data ? node.left : node.right;
-    }
-  }
-
-  getMaxDepth() {
-    return this.depth(this.root);
-  }
-  depth(node) {
-    if (node === null) {
-      return 0;
-    }
-    return Math.max(this.depth(node.left), this.depth(node.right)) + 1;
-  }
-
-  levelOrderTraversal() { // 广度优先遍历
-    const que = [this.root];
-    const list = [];
-    while (que.length) {
-      const node = que.shift();
-      list.push(node.data);
+  // 插入值
+  static _insert (val, node) {
+    if (val < node.val) {
       if (node.left) {
-        que.push(node.left);
+        this._insert(val, node.left)
+      } else {
+        node.left = new Node(val)
+      }
+    } else {
+      if (node.right) {
+        this._insert(val, node.right)
+      } else {
+        node.right = new Node(val)
+      }
+    }
+  }
+
+  insert (val) {
+    if (!this.root) {
+      this.root = new Node(val)
+      return
+    }
+
+    this.constructor._insert(val, this.root)
+  }
+
+  // 挂载的辅助函数
+  static _mountMinTree (tree, host) {
+    if (host.left) {
+      this._mountMinTree(tree, host.left)
+    } else {
+      host.left = tree
+    }
+    return host
+  }
+
+  static _minNode (node) {
+    if (!node) {
+      return null
+    }
+    while (node && node.left) {
+      node = node.left
+    }
+    return node
+  }
+
+  static _remove (val, node) {
+    if (!node) {
+      return null
+    }
+
+    if (val === node.val) { // 要删除的节点是当前节点
+      if (!node.right) { // 右子树不存在，直接用左子树替换当前节点流程结束
+        return node.left
+      } else if (!node.left) {
+        return node.right
+      } else { // 右子树存在
+        // return this._mountMinTree(node.left, node.right); // 取出左子树挂在到右子树的最小节点上，返回该右子树替换当前节点
+        const minNode = this._minNode(node.right)
+        node.val = minNode.val
+        node.right = this._remove(minNode.val, node.right)
+        return node
+      }
+    } else if (val < node.val) { // 要删除的节点在左子树
+      node.left = this._remove(val, node.left)
+      return node
+    } else { // 要删除的节点在右子树
+      node.right = this._remove(val, node.right)
+      return node
+    }
+  }
+
+  // 移除节点
+  remove (val) {
+    this.root = this.constructor._remove(val, this.root)
+  }
+
+  static _includes (val, node) {
+    if (!node) {
+      return false
+    }
+    if (val === node.val) {
+      return true
+    }
+    return this._includes(val, val < node.val ? node.left : node.right)
+  }
+
+  includes (val) {
+    return this.constructor._includes(val, this.root)
+  }
+
+  static depth (node) {
+    if (!node) {
+      return 0
+    }
+    return Math.max(this.depth(node.left), this.depth(node.right)) + 1
+  }
+
+  getMaxDepth () {
+    return this.constructor.depth(this.root)
+  }
+
+  bfs () {
+    if (!this.root) {
+      return []
+    }
+    const queue = [this.root]
+    const list = []
+    while (queue.length) {
+      const node = queue.shift()
+      list.push(node.val)
+      if (node.left) {
+        queue.push(node.left)
       }
       if (node.right) {
-        que.push(node.right);
+        queue.push(node.right)
       }
     }
-    return list;
+    return list
   }
 
-  preOrder(node = this.root, preList = []) { // 深度优先遍历：前序遍历
+  dfsPre (node = this.root, list = []) {
     if (node) {
-      preList.push(node.data);
-      this.preOrder(node.left, preList);
-      this.preOrder(node.right, preList);
+      list.push(node.val)
+      this.dfsPre(node.left, list)
+      this.dfsPre(node.right, list)
     }
-    return preList;
+    return list
   }
 
-  inOrder(node = this.root, inList = []) { // 深度优先遍历：中序遍历
+  dfsIn (node = this.root, list = []) {
     if (node) {
-      this.inOrder(node.left, inList);
-      inList.push(node.data);
-      this.inOrder(node.right, inList);
+      this.dfsIn(node.left, list)
+      list.push(node.val)
+      this.dfsIn(node.right, list)
     }
-
-    return inList;
+    return list
   }
 
-  postOrder(node = this.root, postList = []) { // 深度优先遍历：后序遍历
+  dfsPost (node = this.root, list = []) {
     if (node) {
-      this.postOrder(node.left, postList);
-      this.postOrder(node.right, postList);
-      postList.push(node.data);
+      this.dfsPost(node.left, list)
+      this.dfsPost(node.right, list)
+      list.push(node.val)
     }
-
-    return postList;
+    return list
   }
 }
 
-const tree = new Tree();
+const tree = new Tree()
 
-tree.insert(8);
-tree.insert(14);
-tree.insert(13);
-tree.insert(6);
-tree.insert(3);
-tree.insert(7);
-tree.insert(11);
+tree.insert(8)
+tree.insert(14)
+tree.insert(13)
+tree.insert(6)
+tree.insert(3)
+tree.insert(7)
+tree.insert(11)
 
-// tree.remove(11);
+tree.remove(11)
 
-tree.getMaxDepth();
+tree.getMaxDepth()
 
-console.log(tree);
+console.log(tree)
